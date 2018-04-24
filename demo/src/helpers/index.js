c1e3f7b7b5b703c63cebdf1a7b4670ed4cdd3b56
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2018-04-24 11:07:37 +0300

    add demo project

---
+++ demo/src/helpers/index.js
@@ -0,0 +1,7 @@
+import sum from './sum'
+import srtReverse from './srtReverse'
+
+export default {
+  sum,
+  srtReverse,
+}
---
+++ demo/src/helpers/sum.js
@@ -0,0 +1,5 @@
+const sum = (...arg) => {
+  return arg.reduce((s, e) => s + e)
+}
+
+export default sum
