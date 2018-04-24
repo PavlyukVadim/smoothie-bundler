d57ecd0a2742efe1866435b88f444d1285665760
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2018-04-25 00:05:45 +0300

    update demo

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,5 +1,8 @@
 import data from './data'
 import helpers from './helpers'
+// import _ from 'underscore'
+const _ = require('underscore')._
 
+console.log(_)
 console.log(data)
 console.log(helpers)
